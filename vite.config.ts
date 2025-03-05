fe54a824b15e2595dbfc1127832f49257fb611fc
gouravsasson <sasson.gourav93@gmail.com>
2025-03-05 18:34:32 +0530

    first comit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/widget.tsx'), 
+      name: 'ReactWidget',
+      fileName: 'react-widget-uv',
+      formats: ['iife'], 
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
