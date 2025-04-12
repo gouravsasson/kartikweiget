89d536d969e3bb6e213da9690965d54afacb9fad
gouravsasson <113465390+gouravsasson@users.noreply.github.com>
2025-04-12 17:53:51 +0530

    Update App.tsx
    jksdkj

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Forkartik from "./components/Forkartik";
 import Test from "./components/Test";
 import { useWidgetContext } from "./constexts/WidgetContext";
-
+import RetellaiAgent from "./components/RetellaiAgent";
+import {
+  RoomContext,
+  LiveKitRoom,
+  ConnectionState,
+} from "@livekit/components-react";
+import { useState } from "react";
 function App() {
   const { type } = useWidgetContext();
-  return <>{type === "test" && <Test/>}</>;
+
+  return <>{type === "livekit" && <RetellaiAgent />}</>;
 }
 
 export default App;
